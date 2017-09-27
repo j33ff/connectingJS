e81df3dc4077c86372fefa302f572319d45fdf96
Jeff FitzGerald <j33ff.fitzgerald@gmail.com>
2017-09-27 04:04:46 +0000

    implementing migrations

---
+++ knexfile.js
@@ -0,0 +1,50 @@
+const settings = require("./settings");
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      // filename: './settings.json'
+      database: settings.database,
+      user: settings.user,
+      password: settings.password
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: settings.database,
+      user:     settings.user,
+      password: settings.password
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: settings.database,
+      user:     settings.user,
+      password: settings.password
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
+
+
