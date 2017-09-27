697fc2e232ddfe7339544c93537b3fea85946c9d
Jeff FitzGerald <j33ff.fitzgerald@gmail.com>
2017-09-27 02:09:06 +0000

    adding knex

--- add_person.js
+++ add_person.js
@@ -1,22 +1,32 @@
-const knex = require("knex");
+const settings = require("./settings");
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    user   : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+    // password: '...'
 
-
-
-
-
-knex.select('first_name').from('famous_people')
-.where('id', '>', 0)
-.orWhere('id', '<', 3)
-.asCallback(function(err, rows) {
-  if (err) return console.error(err);
-  knex.select('id').from('famous_people')
-    .whereIn('nickname', _.pluck(rows, 'name'))
-    .asCallback(function(err, rows) {
-      if (err) return console.error(err);
-      console.log(rows);
-    });
+  }
 });
 
+const firstName = process.argv[2];
+const lastName = process.argv[3];
+const dob = process.argv[4];
 
+knex('famous_people').insert({
+  first_name: firstName,
+  last_name: lastName,
+  birthdate: dob
+})
+.returning('id')
+.then((id)=>{
+  console.log("new record inserted "+id);
+});
+
+// ([{first_name: firstName},{last_name: lastName}, {birthdate: dob}]).into('famous_people');
 
 knex.destroy();
