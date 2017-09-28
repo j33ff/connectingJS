770bb76ffa10144425e9bcde29cf784327e2a550
Jeff FitzGerald <j33ff.fitzgerald@gmail.com>
2017-09-28 01:41:57 +0000

    implement famous people foerign key

---
+++ migrations/20170928010102_link_famous.js
@@ -0,0 +1,16 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('milestones', function (table) {
+    // now go modify the variable table - which corresponds to milestones
+      table.integer('famous_people_id').unsigned().index().references('id').inTable('famous_people')
+      // creates famous_people_id as foreign key in table milestones (could put reference('famous_people.id'));
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+
+  ]);
+};
+
